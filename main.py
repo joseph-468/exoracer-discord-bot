9a4e8487d11fb7e583b65cf73a0fbebfa85a9016
Joseph <75801386+joseph-468@users.noreply.github.com>
2022-11-23 01:35:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -81,44 +81,48 @@ async def get_data(ctx, player_name=None, player_number=None):
         await ctx.send("Player not found.")
         return
 
-    # Send data when multiple accounts exist
+    # Runs if multiple players with same name are found and a specific one isn't specified
     if len(player_indexes) > 1 and player_number is None:
-        file = open(str(player_indexes[0]) + ".txt", "w")
-        path = player_indexes[0]
+        path = str(player_indexes[0]) + ".txt"
+        file = open(path, "w")
+        # Store defined fields under player name in file for each player found
         for x, i in enumerate(player_indexes):
             file.write(f"{player_name} {x+1}\n")
             fields = [data[0][i], data[6][i], data[9][i],
                       data[3][i], data[4][i], data[5][i]]
+            # Write fields to convert empty string to avoid errors
             for y, j in enumerate(fields):
                 if not j:
-                    fields[y] = "null"
-            for y, j in enumerate(fields):
+                    j = "null"
                 file.write(f"{data_fields[y]}: {j}\n")
             file.write("\n")
         file.close()
-        await ctx.send(file=discord.File(f"{path}.txt", f"{player_name}.txt"))
-        if os.path.exists(str(player_indexes[0]) + ".txt"):
-            os.remove(str(player_indexes[0]) + ".txt")
+
+        # Sends file, deletes it and returns
+        await ctx.send(file=discord.File(path, f"{player_name}.txt"))
+        if os.path.exists(path):
+            os.remove(path)
         return
+
+    # Runs if player number is specified
     elif player_number is not None:
+        # Returns with error if player number is out of range
         if player_number < 1 or player_number > len(player_indexes):
             await ctx.send("Player not found.")
             return
+        # Removes all other players besides the one specified
         player_indexes = [player_indexes[player_number-1]]
-    # Send data when one account exists
-    embeds = []
-    for x, i in enumerate(player_indexes):
-        embed = discord.Embed(title=player_name, color=0x7244CD)
-        embed_fields = [data[0][i], data[6][i], data[9][i],
-                        data[3][i], data[4][i], data[5][i]]
-        for y, j in enumerate(embed_fields):
-            if not j:
-                embed_fields[y] = "null"
-        for y, j in enumerate(embed_fields):
-            embed.add_field(name=data_fields[y], value=j, inline=False)
-        embeds.append(embed)
-    for embed in embeds:
-        await ctx.send(embed=embed)
+    # Creates embed with defined fields for all players found
+    embed = discord.Embed(title=player_name, color=0x7244CD)
+    embed_fields = [data[0][player_indexes[0]], data[6][player_indexes[0]], data[9][player_indexes[0]],
+                    data[3][player_indexes[0]], data[4][player_indexes[0]], data[5][player_indexes[0]]]
+    # Add fields to embed and converts empty string to avoid errors
+    for y, j in enumerate(embed_fields):
+        if not j:
+            j = "null"
+        embed.add_field(name=data_fields[y], value=j, inline=False)
+    # Send embed
+    await ctx.send(embed=embed)
 
 
 # Import token from env file and run bot
